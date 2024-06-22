837e46662bf47a399b134b05ef9cb1f0e5bed478
Sanjiv Thapa <154358088+sanjivthapasvt@users.noreply.github.com>
2024-06-22 08:54:24 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const yes = document.getElementById("yes");
-const Esewa = document.getElementById("Esewa");
-const Khalti = document.getElementById("Khalti");
-const IMEI_Pay = document.getElementById("IMEI_Pay");
 const mysubmit = document.getElementById("mysubmit");
 const webresult = document.getElementById("webresult");
-const payresult = document.getElementById("payresult");
 const submit = document.getElementById("submit");
 
 submit.onclick = function () {
@@ -15,18 +11,4 @@ submit.onclick = function () {
         webresult.textContent = `I will improve my website in future sorry for disappointing you!!!`
     }
 
-}
-mysubmit.onclick = function () {
-    if (Esewa.checked) {
-        payresult.textContent = `You are paying with Esewa!!!`
-    }
-    else if (Khalti.checked) {
-        payresult.textContent = `You are paying with Khalti!!!`
-    }
-    else if (IMEI_Pay.checked) {
-        payresult.textContent = `You are paying with IMEI Pay!!!`
-    }
-    else {
-        payresult.textContent = `Please select payment method!!!`
-    }
 }
